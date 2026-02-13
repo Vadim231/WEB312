bb75ddd8b4b28b3d59889abf083704f3eeb88f69
StartSoft <leonkiselev07@mail.ru>
2026-02-13 21:08:08 +0700

    class Advert init

--- script.js
+++ script.js
@@ -11,3 +11,11 @@ class Advert{
     }
 }
 let adverts = [];
+
+adverts.push(new Advert("Track 2", 8000, false));
+adverts.push(new Advert("Kingston Fury 32 gb", 35000, false));
+adverts.push(new Advert("Футболка клещ рианна гоблин", 3500, true));
+
+adverts.forEach(function(ad, index){
+    console.log(index, ad.name);
+});
