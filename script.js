2453c22d16103fa9d6ddf6d92fafdf5d4162b9bb
StartSoft <leonkiselev07@mail.ru>
2026-03-04 20:31:32 +0700

    wished

--- script.js
+++ script.js
@@ -13,30 +13,30 @@ class Adverta{
     }
 }
 let adverts = [];
-    const sectionsWithToggle = document.querySelectorAll('.sidemenu .selection.ram-section');
-    sectionsWithToggle.forEach(section => {
-        const showMoreButton = section.querySelector('.show-more-button');
-        const collapseButton = section.querySelector('.collapse-button');
-        const hiddenItems = section.querySelector('.hidden-items');
-        const visibleItems = section.querySelector('.visible-items');
+const sectionsWithToggle = document.querySelectorAll('.sidemenu .selection.ram-section');
+sectionsWithToggle.forEach(section => {
+    const showMoreButton = section.querySelector('.show-more-button');
+    const collapseButton = section.querySelector('.collapse-button');
+    const hiddenItems = section.querySelector('.hidden-items');
+    const visibleItems = section.querySelector('.visible-items');
 
-        if (showMoreButton && collapseButton && hiddenItems && visibleItems) {
-            showMoreButton.addEventListener('click', function() {
-                hiddenItems.style.display = 'block';
-                showMoreButton.style.display = 'none';
-                collapseButton.style.display = 'block';
+    if (showMoreButton && collapseButton && hiddenItems && visibleItems) {
+        showMoreButton.addEventListener('click', function() {
+            hiddenItems.style.display = 'block';
+            showMoreButton.style.display = 'none';
+            collapseButton.style.display = 'block';
+        });
+        collapseButton.addEventListener('click', function() {
+            hiddenItems.style.display = 'none';
+            showMoreButton.style.display = 'block';
+            collapseButton.style.display = 'none';
+            const hiddenCheckboxes = hiddenItems.querySelectorAll('input[type="checkbox"]');
+            hiddenCheckboxes.forEach(checkbox => {
+                checkbox.checked = false;
             });
-            collapseButton.addEventListener('click', function() {
-                hiddenItems.style.display = 'none';
-                showMoreButton.style.display = 'block';
-                collapseButton.style.display = 'none';
-                const hiddenCheckboxes = hiddenItems.querySelectorAll('input[type="checkbox"]');
-                hiddenCheckboxes.forEach(checkbox => {
-                    checkbox.checked = false;
-                });
-            });
-        }
-    });
+        });
+    }
+});
 
 adverts.push(new Adverta("Track 2", 8000, "https://i.siteapi.org/CankSLwN9POoXNIqsccvOz2m8Co=/0x0:962x630/s2.siteapi.org/44b0dfc585a3400/img/3ua6wqve4fcwocog48ogwcw04w8o0k", false));
 adverts.push(new Adverta("Track 2", 8000, "https://i.siteapi.org/CankSLwN9POoXNIqsccvOz2m8Co=/0x0:962x630/s2.siteapi.org/44b0dfc585a3400/img/3ua6wqve4fcwocog48ogwcw04w8o0k", false));
@@ -141,40 +141,71 @@ const changeIsFavorite = function(e, advert){
         e.target.innerText = "♡";
     }
 }
-    adverts.forEach((advert, index) => {
-        mainPage.innerHTML += "<div class='item'>" +
+adverts.forEach((advert, index) => {
+    mainPage.innerHTML += "<div class='item'>" +
+        `<img class='picture' src=${advert.imageUrl} alt="">` +
+        `<a href='about.html' class='aname'>${advert.name}</a>` +
+        `<p class='price'><b>${advert.price} ₽</b></p>` +
+        `<button id='btn${index}'>${advert.isFavorite ? "♥" : "♡"}</button>`+
+        "</div>";
+})
+adverts.forEach((advert, index) => {
+    const btn = document.getElementById(`btn${index}`);
+    btn.addEventListener("click", (e) => changeIsFavorite(e, advert));
+});
+
+wishlist = document.getElementById("wishlist");
+const showWishlist = function(){
+    document.querySelectorAll('.item').forEach(el => el.remove());
+    if(wishlist.checked){
+        adverts.forEach((advert, index) => {
+            if(advert.isFavorite){
+                mainPage.innerHTML += "<div class='item'>" +
+                    `<img class='picture' src=${advert.imageUrl} alt="">` +
+                    `<a href='about.html' class='aname'>${advert.name}</a>` +
+                    `<p class='price'><b>${advert.price} ₽</b></p>` +
+                    `<button id='btn${index}'>♥</button>`+
+                    "</div>";
+            }
+        })
+    }
+    else{
+        adverts.forEach((advert, index) => {
+            mainPage.innerHTML += "<div class='item'>" +
+                `<img class='picture' src=${advert.imageUrl} alt="">` +
+                `<a href='about.html' class='aname'>${advert.name}</a>` +
+                `<p class='price'><b>${advert.price} ₽</b></p>` +
+                `<button id='btn${index}'>${advert.isFavorite ? "♥" : "♡"}</button>`+
+                "</div>";
+        })
+        adverts.forEach((advert, index) => {
+            const btn = document.getElementById(`btn${index}`);
+            btn.addEventListener("click", (e) => changeIsFavorite(e, advert));
+        });
+    }
+}
+wishlist.addEventListener("change", showWishlist);
+const searchInput= document.getElementById('search-input');
+const query = searchInput.value.toLowerCase();
+const resultsContainer = document.getElementById('results-container');
+const mainContainer=document.getElementById('main-page');
+resultsContainer.innerHTML = '';
+if (query === ''){
+    resultsContainer.style.display='none';
+}
+const filteredProducts = adverts.filter(advert => advert.name.toLowerCase().includes(query));
+if(filteredProducts>0) {
+    if (mainContainer) mainContainer.style.display = 'none';
+    filteredProducts.forEach(advert => {
+        const productItem = document.createElement('div');
+        productItem.classList.add('product-item');
+        productItem.innerHTML = "<div class='item'>" +
             `<img class='picture' src=${advert.imageUrl} alt="">` +
-            `<a href='about.html' class='aname'>${advert.name}</a>` +
+            `<p  class='name'>${advert.name}</p>` +
             `<p class='price'><b>${advert.price} ₽</b></p>` +
             `<button id='btn${index}'>${advert.isFavorite ? "♥" : "♡"}</button>`+
             "</div>";
-    })
-    adverts.forEach((advert, index) => {
-        const btn = document.getElementById(`btn${index}`);
-        btn.addEventListener("click", (e) => changeIsFavorite(e, advert));
+        resultsContainer.appendChild(productItem);
     });
-
-    const searchInput= document.getElementById('search-input');
-    const query = searchInput.value.toLowerCase();
-    const resultsContainer = document.getElementById('results-container');
-    const mainContainer=document.getElementById('main-page');
-    resultsContainer.innerHTML = '';
-    if (query === ''){
-        resultsContainer.style.display='none';
-    }
-    const filteredProducts = adverts.filter(advert => advert.name.toLowerCase().includes(query));
-    if(filteredProducts>0) {
-        if (mainContainer) mainContainer.style.display = 'none';
-        filteredProducts.forEach(advert => {
-            const productItem = document.createElement('div');
-            productItem.classList.add('product-item');
-            productItem.innerHTML = "<div class='item'>" +
-                `<img class='picture' src=${advert.imageUrl} alt="">` +
-                `<p  class='name'>${advert.name}</p>` +
-                `<p class='price'><b>${advert.price} ₽</b></p>` +
-                `<button id='btn${index}'>${advert.isFavorite ? "♥" : "♡"}</button>`+
-            "</div>";
-            resultsContainer.appendChild(productItem);
-        });
-    }
+}
 
